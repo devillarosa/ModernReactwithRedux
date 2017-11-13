6e2d5025b1d4773edc870f9235783ef59ac7cd29
EC2 Default User <ec2-user@ip-172-31-7-127.us-west-1.compute.internal>
2017-11-13 00:03:40 +0000

    continual development

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar.js'
-
+import VideoList from './components/video_list.js'
 const API_KEY = '' //Insert API_Key here
 
 
@@ -24,6 +24,7 @@ class App extends Component{
         return (
                 <div>
                     <SearchBar />
+                    <VideoList videos={this.state.videos}/>
                 </div>
                );
     }
